9eca282ab0215bb9320a82413ca9e687806c9b00
elek-tron-ich <sebastian.schaaf@scai.fraunhofer.de>
2017-02-22 10:29:29 +0000

    add requirements file

---
+++ setup.py
@@ -0,0 +1,58 @@
+import setuptools
+import os
+import codecs
+
+KEYWORDS = ['Testing', 'Devops']
+CLASSIFIERS = [
+    'Development Status :: 1 - Planning',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: Apache Software License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.6',
+    'Topic :: Scientific/Engineering :: Bio-Informatics'
+]
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_long_description():
+    """Get the long_description from the README.rst file. Assume UTF-8 encoding."""
+    with codecs.open(os.path.join(HERE, 'README.rst'), encoding='utf-8') as f:
+        long_description = f.read()
+    return long_description
+
+if __name__ == '__main__':
+    setuptools.setup(
+        name='SCAInet',
+        version='0.1.0-dev',
+        author='Sebastian Schaaf',
+        author_email='',
+        description='If Dr. Frankenstein were an arthropod, and also a python package',
+        license='GPLv3',
+        url='https://github.com/elek-tron-ich/workshop',
+
+        # Look for python code inside /src/
+        packages=setuptools.find_packages(where='src'),
+
+        # Assign the package-level folder ('') to be the one it finds in 'src'
+        package_dir={'': 'src'},
+
+        install_requires=[
+            'click',
+        ],
+
+        # Add command line function to automatically run the main function from animated_barnacle.cli
+        entry_points={
+            'console_scripts': [
+                'animated_barnacle = animated_barnacle.cli:main',
+            ]
+        },
+
+        long_description=get_long_description(),
+        classifiers=CLASSIFIERS,
+        keywords=KEYWORDS,
+    )
--- src/SCAInet/__init__.py
+++ src/SCAInet/__init__.py
@@ -1,1 +1,12 @@
 __version__ = '0.1.0-dev'
+
+__title__ = ''
+__description__ = ''
+__url__ = ''
+
+__author__ = ''
+__email__ = ''
+
+__license__ = ''
+__copyright__ = ''
+
